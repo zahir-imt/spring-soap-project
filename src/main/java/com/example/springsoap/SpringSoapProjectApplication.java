62354e674d7420ba7b293308439f72150bb2dd44
Dewan-Iftesham <dewaniftesham23@gmail.com>
2025-12-27 16:01:18 -0500

    Add Spring Boot main application class

---
+++ src/main/java/com/example/springsoap/SpringSoapProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.springsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoapProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSoapProjectApplication.class, args);
+    }
+}
