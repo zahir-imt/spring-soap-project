58c6b5f51fbe65f24fc33aec628a7d3cd172e3fc
Dewan-Iftesham <dewaniftesham23@gmail.com>
2026-03-09 17:31:35 -0400

    Add base SOAP configuration class
    Added a base configuration class for SOAP web services.

---
+++ src/main/java/com/example/springsoap/config/SoapConfig.java
@@ -0,0 +1,11 @@
+package com.example.springsoap.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SoapConfig {
+
+    // Base configuration class for SOAP web service setup
+    // Future SOAP endpoints and WSDL configurations will be added here
+
+}
